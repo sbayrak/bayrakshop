0385921dd218b8de39a86589c5da88a62ea12ae5
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-07-05 18:20:59 +0300

    added hero component to Index page

---
+++ components/hero/Hero.js
@@ -0,0 +1,140 @@
+// @@@ MATERIAL-UI @@@
+import { Typography, Button, Box, Container } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
+import hero from '../../public/hero.png';
+// @@@ MATERIAL-UI @@@
+
+// @@@ nextjs @@@
+import Image from 'next/image';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { signIn, signOut, useSession } from 'next-auth/client';
+// @@@ nextjs @@@
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '100%',
+    border: '1px solid red',
+    height: '400vh',
+    marginTop: theme.spacing(15),
+    [theme.breakpoints.down('sm')]: {
+      marginTop: theme.spacing(0),
+    },
+  },
+  hero: {
+    position: 'relative',
+  },
+  imgWrapper: {
+    position: 'relative',
+    width: '100%',
+    height: '700px',
+    [theme.breakpoints.down('sm')]: {
+      height: '200px',
+    },
+  },
+  img: {},
+  heroTypos: {
+    position: 'absolute',
+    top: 250,
+    right: 250,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: theme.spacing(1),
+    [theme.breakpoints.down('sm')]: {
+      top: 50,
+      right: 0,
+    },
+  },
+  Typo1: {
+    fontWeight: theme.typography.fontWeightMedium,
+    color: '#f6f6f6',
+    marginBottom: theme.spacing(6),
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '18px',
+      marginBottom: theme.spacing(2),
+    },
+  },
+  Typo2: {},
+  Typo2Link: {
+    display: 'flex',
+    alignItems: 'center',
+    textDecoration: 'none',
+    borderRadius: '5px',
+    padding: theme.spacing(1),
+    paddingLeft: theme.spacing(2),
+    paddingRight: theme.spacing(2),
+    marginBottom: theme.spacing(1),
+    backgroundColor: '#f6f6f6',
+    color: '#5652de',
+    textAlign: 'center',
+    fontWeight: theme.typography.fontWeightMedium,
+    marginBottom: theme.spacing(3),
+    boxShadow: '1px 1px 10px 1px rgba(233,233,233,0.29)',
+    transition: '0.3s ease',
+    '&:hover': {
+      backgroundColor: '#fff',
+      color: '#5652de',
+      boxShadow: '1px 1px 11px 2px rgba(233,233,233,0.59)',
+    },
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '12px',
+      padding: theme.spacing(0.5),
+      paddingLeft: theme.spacing(1),
+      paddingRight: theme.spacing(1),
+      marginBottom: theme.spacing(0),
+    },
+  },
+  Typo2Icon: {
+    [theme.breakpoints.down('sm')]: {
+      fontSize: '14px',
+    },
+  },
+}));
+
+const Hero = () => {
+  const classes = useStyles();
+  const [session, loading] = useSession();
+  const router = useRouter();
+
+  const customSignOut = async () => {
+    const data = await signOut({
+      redirect: false,
+      callbackUrl: `${window.location.origin}/`,
+    });
+    router.push(data.url);
+  };
+  console.log(session);
+  return (
+    <Box component='div' className={classes.root}>
+      <div className={classes.hero}>
+        <div className={classes.imgWrapper}>
+          <Image
+            src={hero}
+            className={classes.img}
+            alt='koslowshop'
+            layout='fill'
+            placeholder='blur'
+            objectFit='cover'
+          ></Image>
+        </div>
+        <div className={classes.heroTypos}>
+          <Typography variant='h2' className={classes.Typo1}>
+            Handmade desserts.
+          </Typography>
+          <Typography variant='h5' className={classes.Typo2}>
+            <Link href='#!'>
+              <a className={classes.Typo2Link}>
+                Shop Now &nbsp;
+                <ArrowForwardIosIcon className={classes.Typo2Icon} />
+              </a>
+            </Link>
+          </Typography>
+        </div>
+      </div>
+    </Box>
+  );
+};
+
+export default Hero;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
-import Head from 'next/head';
-import { Typography, Button, Box } from '@material-ui/core';
-import { connectToDatabase } from '../util/mongodb';
+// @@@ nextjs @@@
+import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { signIn, signOut, useSession } from 'next-auth/client';
+// @@@ nextjs @@@
 
-export default function Home({ isConnected }) {
+// @@@ COMPONENT IMPORTS @@@
+import Hero from '../components/hero/Hero';
+// @@@ COMPONENT IMPORTS @@@
+
+export default function Home() {
   const [session, loading] = useSession();
   const router = useRouter();
 
@@ -17,35 +21,5 @@ export default function Home({ isConnected }) {
     router.push(data.url);
   };
   console.log(session);
-  return (
-    <div className='container'>
-      <Head>
-        <title>Create Next App</title>
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
-      <Box style={{ height: '80vh' }}></Box>
-      <Link href='/auth/signup'>
-        <a>Sign Up</a>
-      </Link>{' '}
-      &nbsp;
-      <Link href='/auth/password-reset'>
-        <a>Forgot password</a>
-      </Link>
-      <Button onClick={() => signIn()}>Sign In</Button>
-      <br /> <br />
-      {session && ` welcome! ${session.user.email}`}
-      <br /> <br />
-      {session && <Button onClick={customSignOut}>Sign Out</Button>}
-    </div>
-  );
-}
-
-export async function getServerSideProps(context) {
-  const { client } = await connectToDatabase();
-
-  const isConnected = await client.isConnected();
-
-  return {
-    props: { isConnected },
-  };
+  return <Hero></Hero>;
 }
