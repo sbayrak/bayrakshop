553a12f869e5221bcbacad19cfe983faf0ac8d31
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-07-10 17:43:42 +0300

    added add new product page

--- pages/dashboard/products/add-new-product.js
+++ pages/dashboard/products/add-new-product.js
@@ -1,5 +1,14 @@
 // @@@ MATERIAL-UI @@@
-import { Box, Container, Grid, Typography } from '@material-ui/core';
+import {
+  Box,
+  Container,
+  Grid,
+  Typography,
+  TextField,
+  Switch,
+  Button,
+  InputAdornment,
+} from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 // @@@ MATERIAL-UI @@@
 
@@ -10,7 +19,67 @@ import { useState } from 'react';
 import DashboardLeft from '../../../components/dashboard/DashboardLeft';
 // @@@ nextjs @@@
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  input: {
+    display: 'none',
+  },
+  gridContainer: {
+    borderRadius: '5px',
+    backgroundColor: '#fff',
+    padding: theme.spacing(5),
+    marginTop: theme.spacing(20),
+    boxShadow: theme.shadows[1],
+  },
+  Typo1: {
+    marginBottom: theme.spacing(4),
+    color: theme.palette.grey[800],
+  },
+  gridFormItem: {
+    marginBottom: theme.spacing(8),
+  },
+  gridFormItemTopLeft: {
+    paddingRight: theme.spacing(4),
+  },
+  gridFormItemTopRight: {
+    paddingLeft: theme.spacing(4),
+  },
+  switchWrapper: {
+    border: '1px solid rgba(0,0,0,0.2)',
+    borderRadius: '5px',
+    padding: theme.spacing(2),
+  },
+  activeTypo: {
+    marginRight: theme.spacing(5),
+  },
+  imageWrapper: {
+    display: 'flex',
+    border: '1px solid rgba(0,0,0,0.2)',
+    borderRadius: '5px',
+    padding: theme.spacing(2),
+  },
+  submitBtnWrapper: {
+    marginTop: theme.spacing(5),
+  },
+}));
+
 const AddNewProduct = () => {
+  const classes = useStyles();
+  const [name, setName] = useState('');
+  const [price, setPrice] = useState('');
+  const [description, setDescription] = useState('');
+  const [quantity, setQuantity] = useState('');
+  const [switchState, setSwitchState] = useState(false);
+
+  const handleChange = (event) => {
+    setSwitchState(!switchState);
+  };
+
+  console.log(switchState);
   return (
     <>
       <Box component='div'>
@@ -18,8 +87,138 @@ const AddNewProduct = () => {
           <Grid item md={3}>
             <DashboardLeft></DashboardLeft>
           </Grid>
-          <Grid item md={9}>
-            <Typography>asd</Typography>
+          <Grid item md={8}>
+            <Grid container className={classes.gridContainer}>
+              <Grid item md={12}>
+                <Typography variant='h4' className={classes.Typo1}>
+                  Add New Product
+                </Typography>
+              </Grid>
+              <Grid container item md={12}>
+                <form>
+                  <Grid container>
+                    <Grid
+                      item
+                      md={6}
+                      className={`${classes.gridFormItem} ${classes.gridFormItemTopLeft}`}
+                    >
+                      <Typography variant='h6'>Name</Typography>
+                      <TextField
+                        variant='outlined'
+                        fullWidth
+                        helperText='*Please enter name of the product'
+                        color='primary'
+                      ></TextField>
+                    </Grid>
+                    <Grid
+                      item
+                      md={6}
+                      className={`${classes.gridFormItem} ${classes.gridFormItemTopRight}`}
+                    >
+                      <Typography variant='h6'>Price</Typography>
+                      <TextField
+                        variant='outlined'
+                        fullWidth
+                        helperText='*Please enter price of the product'
+                        color='primary'
+                      ></TextField>
+                    </Grid>
+                    <Grid item md={12} className={classes.gridFormItem}>
+                      <Typography variant='h6'>Description</Typography>
+                      <TextField
+                        variant='outlined'
+                        multiline
+                        fullWidth
+                        color='primary'
+                        rows={4}
+                        helperText='*Please enter a relavant description of the product'
+                      ></TextField>
+                    </Grid>
+                    <Grid
+                      item
+                      md={6}
+                      className={`${classes.gridFormItem} ${classes.gridFormItemTopLeft}`}
+                    >
+                      <Typography variant='h6'>Quantity</Typography>
+                      <TextField
+                        variant='outlined'
+                        fullWidth
+                        color='primary'
+                        helperText='*Please enter the quantity of the product or leave it empty'
+                      ></TextField>
+                    </Grid>
+                    <Grid item md={6}></Grid>
+
+                    <Grid
+                      item
+                      md={6}
+                      className={`${classes.gridFormItem} ${classes.switchWrapper}`}
+                    >
+                      <div
+                        style={{
+                          display: 'flex',
+                          marginBottom: '10px',
+                        }}
+                      >
+                        <Typography variant='h6' className={classes.activeTypo}>
+                          Active
+                        </Typography>
+                        <Switch
+                          checked={switchState}
+                          onChange={handleChange}
+                          size='medium'
+                          color='primary'
+                          name='switchState'
+                          inputProps={{ 'aria-label': 'primary checkbox' }}
+                        />
+                      </div>
+                      <Typography variant='caption' color='textSecondary'>
+                        *Turn it on if you want to make the product live
+                      </Typography>
+                    </Grid>
+                    <Grid item md={6}></Grid>
+                    <Grid
+                      item
+                      md={6}
+                      className={`${classes.gridFormItem} ${classes.imageWrapper}`}
+                    >
+                      <Typography variant='h6' className={classes.activeTypo}>
+                        Image
+                      </Typography>
+                      <div className={classes.root}>
+                        <input
+                          accept='image/*'
+                          className={classes.input}
+                          id='contained-button-file'
+                          multiple
+                          type='file'
+                        />
+                        <label htmlFor='contained-button-file'>
+                          <Button
+                            variant='contained'
+                            color='primary'
+                            component='span'
+                            disableElevation
+                          >
+                            Upload
+                          </Button>
+                        </label>
+                      </div>
+                    </Grid>
+                    <Grid item md={6}></Grid>
+                    <Grid
+                      item
+                      md={6}
+                      className={`${classes.gridFormItem} ${classes.submitBtnWrapper}`}
+                    >
+                      <Button variant='contained' color='primary' fullWidth>
+                        submit
+                      </Button>
+                    </Grid>
+                  </Grid>
+                </form>
+              </Grid>
+            </Grid>
           </Grid>
         </Grid>
       </Box>
