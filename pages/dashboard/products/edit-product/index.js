f3330379e254d70b37b5ee9dbe04788e4b7de457
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-07-13 12:21:04 +0300

    added contidions if user enter non exist product

--- pages/dashboard/products/edit-product/index.js
+++ pages/dashboard/products/edit-product/index.js
@@ -16,11 +16,13 @@ import DashboardLeft from '../../../../components/dashboard/DashboardLeft';
 import CheckCircleOutlineIcon from '@material-ui/icons/CheckCircleOutline';
 import CloudUploadIcon from '@material-ui/icons/CloudUpload';
 import CancelIcon from '@material-ui/icons/Cancel';
+import ErrorOutlineIcon from '@material-ui/icons/ErrorOutline';
 // @@@ MATERIAL-UI @@@
 
 // @@@ nextjs @@@
-import Link from 'next/link';
+import { useRouter } from 'next/router';
 import Image from 'next/image';
+import Link from 'next/link';
 import { useState, useEffect } from 'react';
 import { connectToDatabase } from '../../../../util/mongodb';
 import { ObjectId } from 'mongodb';
@@ -30,15 +32,48 @@ import { ObjectId } from 'mongodb';
 export const getServerSideProps = async (context) => {
   const urlQuery = context.query;
   const { db } = await connectToDatabase();
+  let resultProduct;
+  let noProduct = false;
 
-  const getProduct = await db
-    .collection('products')
-    .findOne({ _id: ObjectId(urlQuery.id) });
+  if (!urlQuery.id) {
+    resultProduct = {
+      name: '',
+      price: '',
+      description: '',
+      quantity: '',
+      image: [],
+    };
+    noProduct = true;
+  } else if (urlQuery.id.length < 24) {
+    resultProduct = {
+      name: '',
+      price: '',
+      description: '',
+      quantity: '',
+      image: [],
+    };
+    noProduct = true;
+  } else {
+    const getProduct = await db
+      .collection('products')
+      .findOne({ _id: ObjectId(urlQuery.id) });
+    resultProduct = await JSON.parse(JSON.stringify(getProduct)); // JSONify the object is required for serialize
+  }
 
-  const resultProduct = await JSON.parse(JSON.stringify(getProduct)); // JSONify the object is required for serialize
-
+  if (!resultProduct) {
+    resultProduct = {
+      name: '',
+      price: '',
+      description: '',
+      quantity: '',
+      image: [],
+    };
+    noProduct = true;
+  }
   return {
     props: {
+      noProduct,
+      urlQuery,
       resultProduct,
     },
   };
@@ -133,10 +168,29 @@ const useStyles = makeStyles((theme) => ({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  productErrorWrapper: {
+    border: '1px solid red',
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(3),
+    borderRadius: '5px',
+    backgroundColor: 'rgba(244,67,54,0.85)',
+  },
+  productErrorTypo: {
+    marginLeft: theme.spacing(1),
+  },
+  productErrorLink: {
+    color: '#f6f6f6',
+    textDecoration: 'none',
+    fontWeight: theme.typography.fontWeightBold,
+    paddingBottom: '1px',
+    borderBottom: '1px solid #f6f6f6',
+  },
 }));
 
-const EditProduct = ({ resultProduct }) => {
+const EditProduct = ({ resultProduct, urlQuery, noProduct }) => {
   const classes = useStyles();
+  const router = useRouter();
   const [name, setName] = useState('');
   const [errorName, setErrorName] = useState(false);
   const [price, setPrice] = useState('');
@@ -150,8 +204,12 @@ const EditProduct = ({ resultProduct }) => {
   const [successProduct, setSuccessProduct] = useState(false);
   const [snackbar, setSnackbar] = useState(true);
   const [spinner, setSpinner] = useState(false);
+  const [productError, setProductError] = useState(false);
 
   useEffect(() => {
+    if (!urlQuery.id || noProduct) {
+      setProductError(true);
+    }
     if (resultProduct) {
       setName(resultProduct.name);
       setPrice(resultProduct.price);
@@ -255,6 +313,208 @@ const EditProduct = ({ resultProduct }) => {
     setUploadedImages([...uploadedImages, result]); // ADD NEW UPLOADED IMG TO uploadedImages STATE
   };
 
+  const editProductContainer = (
+    <Grid container item md={12} xs={12}>
+      <form onSubmit={editProductHandler}>
+        <Grid container>
+          <Grid
+            item
+            md={6}
+            xs={12}
+            className={`${classes.gridFormItem} ${classes.gridFormItemTopLeft}`}
+          >
+            <Typography variant='h6' className={classes.formItemTypo}>
+              Name
+            </Typography>
+            <TextField
+              variant='outlined'
+              fullWidth
+              color='primary'
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              error={errorName}
+            ></TextField>
+          </Grid>
+          <Grid
+            item
+            md={6}
+            xs={11}
+            className={`${classes.gridFormItem} ${classes.gridFormItemTopRight}`}
+          >
+            <Typography variant='h6' className={classes.formItemTypo}>
+              Price
+            </Typography>
+            <TextField
+              variant='outlined'
+              fullWidth
+              color='primary'
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              error={errorPrice}
+            ></TextField>
+          </Grid>
+          <Grid item md={12} className={classes.gridFormItem}>
+            <Typography variant='h6' className={classes.formItemTypo}>
+              Description
+            </Typography>
+            <TextField
+              variant='outlined'
+              multiline
+              fullWidth
+              color='primary'
+              rows={4}
+              value={description}
+              onChange={(e) => setDescription(e.target.value)}
+              error={errorDescription}
+            ></TextField>
+          </Grid>
+          <Grid
+            item
+            md={6}
+            className={`${classes.gridFormItem} ${classes.gridFormItemTopLeft}`}
+          >
+            <Typography variant='h6' className={classes.formItemTypo}>
+              Quantity
+            </Typography>
+            <TextField
+              variant='outlined'
+              fullWidth
+              color='primary'
+              value={quantity}
+              onChange={(e) => setQuantity(e.target.value)}
+            ></TextField>
+          </Grid>
+          <Grid item md={6}></Grid>
+
+          <Grid
+            item
+            md={6}
+            className={`${classes.gridFormItem} ${classes.switchWrapper}`}
+          >
+            <div
+              style={{
+                display: 'flex',
+                marginBottom: '10px',
+              }}
+            >
+              <Typography
+                variant='h6'
+                className={`${classes.activeTypo} ${classes.formItemTypo}`}
+              >
+                Active
+              </Typography>
+              <Switch
+                checked={switchState}
+                onChange={handleChange}
+                size='medium'
+                color='primary'
+                name='switchState'
+                inputProps={{
+                  'aria-label': 'primary checkbox',
+                }}
+              />
+            </div>
+          </Grid>
+          <Grid item md={6}></Grid>
+
+          <Grid
+            item
+            md={6}
+            xs={11}
+            className={`${classes.gridFormItem} ${classes.submitBtnWrapper}`}
+          >
+            <Button
+              variant='contained'
+              color='primary'
+              fullWidth
+              disabled={uploadedImages.length > 0 ? false : true}
+              type='submit'
+            >
+              submit
+            </Button>
+          </Grid>
+        </Grid>
+      </form>
+      <Grid
+        item
+        md={6}
+        xs={12}
+        className={`${classes.gridFormItem} ${classes.imageWrapper}`}
+      >
+        <Typography variant='h6' className={classes.activeTypo}>
+          Image
+        </Typography>
+        <div className={classes.root}>
+          <form
+            onSubmit={newImageUploadHandler}
+            style={{
+              display: 'flex',
+              justifyContent: 'space-between',
+              alignItems: 'center',
+            }}
+          >
+            <div style={{ marginRight: '20px' }}>
+              <input
+                accept='image/*'
+                className={classes.input}
+                id='contained-button-file'
+                multiple
+                type='file'
+                onChange={(e) => setImageState(e.target.files[0])}
+              />
+              <label htmlFor='contained-button-file'>
+                <Button
+                  variant='contained'
+                  color='primary'
+                  component='span'
+                  disableElevation
+                  onChange={(e) => setImageState(e.target.files[0])}
+                >
+                  Select
+                </Button>
+              </label>
+            </div>
+            <Button
+              variant='contained'
+              color='primary'
+              fullWidth
+              type='submit'
+              disableElevation
+              startIcon={<CloudUploadIcon />}
+            >
+              {spinner ? (
+                <CircularProgress color='secondary' fontSize='small' />
+              ) : (
+                'Upload'
+              )}
+            </Button>
+          </form>
+        </div>
+      </Grid>
+      <Grid item md={6}></Grid>
+      <Grid item md={6} xs={12}>
+        {uploadedImages &&
+          uploadedImages.map((img) => (
+            <div data-id={`${img.asset_id}`} className={classes.imgWrapper}>
+              <Image
+                src={img.secure_url}
+                width={256}
+                height={56}
+                key={img.asset_id}
+              />{' '}
+              <IconButton
+                data-id={`${img.asset_id}`}
+                onClick={removeUploadedImageHandler}
+              >
+                <CancelIcon style={{ color: '#F44336' }} />
+              </IconButton>
+            </div>
+          ))}
+      </Grid>
+      <Grid item md={6}></Grid>
+    </Grid>
+  );
+
   return (
     <>
       <Box component='div'>
@@ -289,211 +549,28 @@ const EditProduct = ({ resultProduct }) => {
                   Edit Product
                 </Typography>
               </Grid>
-              <Grid container item md={12} xs={12}>
-                <form onSubmit={editProductHandler}>
-                  <Grid container>
-                    <Grid
-                      item
-                      md={6}
-                      xs={12}
-                      className={`${classes.gridFormItem} ${classes.gridFormItemTopLeft}`}
+              {productError ? (
+                <Grid item md={12}>
+                  <Box component='div' className={classes.productErrorWrapper}>
+                    <ErrorOutlineIcon color='secondary' />
+                    <Typography
+                      variant='body2'
+                      className={classes.productErrorTypo}
+                      color='secondary'
                     >
-                      <Typography variant='h6' className={classes.formItemTypo}>
-                        Name
-                      </Typography>
-                      <TextField
-                        variant='outlined'
-                        fullWidth
-                        color='primary'
-                        value={name}
-                        onChange={(e) => setName(e.target.value)}
-                        error={errorName}
-                      ></TextField>
-                    </Grid>
-                    <Grid
-                      item
-                      md={6}
-                      xs={11}
-                      className={`${classes.gridFormItem} ${classes.gridFormItemTopRight}`}
-                    >
-                      <Typography variant='h6' className={classes.formItemTypo}>
-                        Price
-                      </Typography>
-                      <TextField
-                        variant='outlined'
-                        fullWidth
-                        color='primary'
-                        value={price}
-                        onChange={(e) => setPrice(e.target.value)}
-                        error={errorPrice}
-                      ></TextField>
-                    </Grid>
-                    <Grid item md={12} className={classes.gridFormItem}>
-                      <Typography variant='h6' className={classes.formItemTypo}>
-                        Description
-                      </Typography>
-                      <TextField
-                        variant='outlined'
-                        multiline
-                        fullWidth
-                        color='primary'
-                        rows={4}
-                        value={description}
-                        onChange={(e) => setDescription(e.target.value)}
-                        error={errorDescription}
-                      ></TextField>
-                    </Grid>
-                    <Grid
-                      item
-                      md={6}
-                      className={`${classes.gridFormItem} ${classes.gridFormItemTopLeft}`}
-                    >
-                      <Typography variant='h6' className={classes.formItemTypo}>
-                        Quantity
-                      </Typography>
-                      <TextField
-                        variant='outlined'
-                        fullWidth
-                        color='primary'
-                        value={quantity}
-                        onChange={(e) => setQuantity(e.target.value)}
-                      ></TextField>
-                    </Grid>
-                    <Grid item md={6}></Grid>
-
-                    <Grid
-                      item
-                      md={6}
-                      className={`${classes.gridFormItem} ${classes.switchWrapper}`}
-                    >
-                      <div
-                        style={{
-                          display: 'flex',
-                          marginBottom: '10px',
-                        }}
-                      >
-                        <Typography
-                          variant='h6'
-                          className={`${classes.activeTypo} ${classes.formItemTypo}`}
-                        >
-                          Active
-                        </Typography>
-                        <Switch
-                          checked={switchState}
-                          onChange={handleChange}
-                          size='medium'
-                          color='primary'
-                          name='switchState'
-                          inputProps={{
-                            'aria-label': 'primary checkbox',
-                          }}
-                        />
-                      </div>
-                    </Grid>
-                    <Grid item md={6}></Grid>
-
-                    <Grid
-                      item
-                      md={6}
-                      xs={11}
-                      className={`${classes.gridFormItem} ${classes.submitBtnWrapper}`}
-                    >
-                      <Button
-                        variant='contained'
-                        color='primary'
-                        fullWidth
-                        disabled={uploadedImages.length > 0 ? false : true}
-                        type='submit'
-                      >
-                        submit
-                      </Button>
-                    </Grid>
-                  </Grid>
-                </form>
-                <Grid
-                  item
-                  md={6}
-                  xs={12}
-                  className={`${classes.gridFormItem} ${classes.imageWrapper}`}
-                >
-                  <Typography variant='h6' className={classes.activeTypo}>
-                    Image
-                  </Typography>
-                  <div className={classes.root}>
-                    <form
-                      onSubmit={newImageUploadHandler}
-                      style={{
-                        display: 'flex',
-                        justifyContent: 'space-between',
-                        alignItems: 'center',
-                      }}
-                    >
-                      <div style={{ marginRight: '20px' }}>
-                        <input
-                          accept='image/*'
-                          className={classes.input}
-                          id='contained-button-file'
-                          multiple
-                          type='file'
-                          onChange={(e) => setImageState(e.target.files[0])}
-                        />
-                        <label htmlFor='contained-button-file'>
-                          <Button
-                            variant='contained'
-                            color='primary'
-                            component='span'
-                            disableElevation
-                            onChange={(e) => setImageState(e.target.files[0])}
-                          >
-                            Select
-                          </Button>
-                        </label>
-                      </div>
-                      <Button
-                        variant='contained'
-                        color='primary'
-                        fullWidth
-                        type='submit'
-                        disableElevation
-                        startIcon={<CloudUploadIcon />}
-                      >
-                        {spinner ? (
-                          <CircularProgress
-                            color='secondary'
-                            fontSize='small'
-                          />
-                        ) : (
-                          'Upload'
-                        )}
-                      </Button>
-                    </form>
-                  </div>
+                      Error! The product you wish to edit could not find or does
+                      not exist. Please go to{' '}
+                      <Link href='/dashboard/products'>
+                        <a className={classes.productErrorLink}>products</a>
+                      </Link>{' '}
+                      page then click on edit button which product you wish to
+                      edit.
+                    </Typography>
+                  </Box>
                 </Grid>
-                <Grid item md={6}></Grid>
-                <Grid item md={6} xs={12}>
-                  {uploadedImages &&
-                    uploadedImages.map((img) => (
-                      <div
-                        data-id={`${img.asset_id}`}
-                        className={classes.imgWrapper}
-                      >
-                        <Image
-                          src={img.secure_url}
-                          width={256}
-                          height={56}
-                          key={img.asset_id}
-                        />{' '}
-                        <IconButton
-                          data-id={`${img.asset_id}`}
-                          onClick={removeUploadedImageHandler}
-                        >
-                          <CancelIcon style={{ color: '#F44336' }} />
-                        </IconButton>
-                      </div>
-                    ))}
-                </Grid>
-                <Grid item md={6}></Grid>
-              </Grid>
+              ) : (
+                editProductContainer
+              )}
             </Grid>
           </Grid>
         </Grid>
