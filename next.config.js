e803cedf0dea767cf3849130b6dc16665de4f979
sbayrak <34121748+sbayrak@users.noreply.github.com>
2021-08-25 13:11:26 +0300

    edited next config 3

--- next.config.js
+++ next.config.js
@@ -2,19 +2,4 @@ module.exports = {
   images: {
     domains: ['res.cloudinary.com'],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      // don't resolve 'fs' module on the client to prevent this error on build --> Error: Can't resolve 'fs'
-      config.resolve.fallback = {
-        fs: false,
-      };
-    }
-
-    return config;
-  },
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
 };
